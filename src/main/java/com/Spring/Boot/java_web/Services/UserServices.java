0ec1357bca744a36b5e3cd9228ac39231f9be8d1
Eduardo <eduardo00valiati@gmail.com>
2021-02-25 11:26:15 -0300

    Service layer, component registration

---
+++ src/main/java/com/Spring/Boot/java_web/Services/UserServices.java
@@ -0,0 +1,30 @@
+package com.Spring.Boot.java_web.Services;
+
+import com.Spring.Boot.java_web.Repository.Repository;
+import com.Spring.Boot.java_web.entitits.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+import java.util.List;
+import java.util.Optional;
+
+//REGISTRAR CLASS COMO UM COMPONENTE DO SPRING '@cOMPONENT'
+//Assim ele podera ser injetado automaticamente com @alrtward
+@Component//Ou @Services como é um serviço questão de semantica
+public class UserServices {
+
+    //injeção de dependencia
+    @Autowired
+    private Repository repository;
+
+    //metodo para retornar todos os usuarios do banco de dados
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    // recuperando um usuário por ID
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
--- src/main/java/com/Spring/Boot/java_web/resource/user/Resource.java
+++ src/main/java/com/Spring/Boot/java_web/resource/user/Resource.java
@@ -1,19 +1,38 @@
 package com.Spring.Boot.java_web.resource.user;
 
+import com.Spring.Boot.java_web.Repository.Repository;
+import com.Spring.Boot.java_web.Services.UserServices;
 import com.Spring.Boot.java_web.entitits.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class Resource {
+    @Autowired
+    private UserServices services;
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail", "99999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = services.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    //ENDIPOINT para buscar um usuário por id
+    @GetMapping(value = "/{id}") //Sgnifica que a url vai aceitar um valor valor
+    //do tipo User por que vai receber apenas um usuário
+    //@PathVariavle para o findById aceitar o Id na url
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = services.findById(id);
+        //.ok é para indicar que teve sucesso
+        // e no corpo da requisição passa o objeto obj
+        return ResponseEntity.ok().body(obj);
     }
 }
