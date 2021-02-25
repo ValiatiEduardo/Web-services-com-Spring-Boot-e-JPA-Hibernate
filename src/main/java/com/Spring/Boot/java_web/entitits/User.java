e54baf887f05ba6e90f3847f8c09a76cd5a4efad
Eduardo <eduardo00valiati@gmail.com>
2021-02-24 22:55:38 -0300

    H2 database, test profile, JPA

--- src/main/java/com/Spring/Boot/java_web/entitits/User.java
+++ src/main/java/com/Spring/Boot/java_web/entitits/User.java
@@ -1,10 +1,18 @@
 package com.Spring.Boot.java_web.entitits;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID = 1l;
 
+    @Id
+    //auto incremento do id no banco de dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
