6f43eb4e0efac1745841a80e57753c4d946baf19
Eduardo <eduardo00valiati@gmail.com>
2021-02-24 23:45:23 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/Spring/Boot/java_web/Repository/Repository.java
+++ src/main/java/com/Spring/Boot/java_web/Repository/Repository.java
@@ -1,2 +1,9 @@
-package com.Spring.Boot.java_web.Repository;public class Repository {
+package com.Spring.Boot.java_web.Repository;
+
+import com.Spring.Boot.java_web.entitits.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public  interface Repository  extends JpaRepository<User, Long> {
+
 }
--- src/main/java/com/Spring/Boot/java_web/config/TestConfig.java
+++ src/main/java/com/Spring/Boot/java_web/config/TestConfig.java
@@ -1,2 +1,31 @@
-package com.Spring.Boot.java_web.config;public class TestConfig {
+package com.Spring.Boot.java_web.config;
+
+import com.Spring.Boot.java_web.Repository.Repository;
+import com.Spring.Boot.java_web.entitits.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private Repository Repository;
+//Assim que o programa inicialialisar os usuarios seram criados
+    //usando commandLineRnner e o metodo run
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        // salvando os dados o banco de dados
+        Repository.saveAll(Arrays.asList(u1, u2));
+
+
+    }
 }
